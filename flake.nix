abff72d52416dd6751f65c61bc13235e89ecf321
xgroleau 🐢 <xavgroleau@gmail.com>
2022-08-18 15:54:09 -0400

    added flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Rust dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustpkg = pkgs.rust-bin.stable.latest.default;
+
+      in with pkgs; { devShell = mkShell { buildInputs = [ rustpkg ]; }; });
+}
