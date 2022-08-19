4bda38b190a7e860c4ff53ca992d56f67dea7011
xgroleau 🐢 <xavgroleau@gmail.com>
2022-08-19 12:09:20 -0400

    Async wip

--- device-register-async/src/lib.rs
+++ device-register-async/src/lib.rs
@@ -1,88 +1,92 @@
-// #![no_std]
-// #![deny(unsafe_code, missing_docs)]
+//! Async version of device-register-async. Must be used with rust unstable
+#![no_std]
+#![deny(unsafe_code, missing_docs)]
+#![feature(generic_associated_types)]
+#![feature(type_alias_impl_trait)]
 
-// /// Traits that define how to read and write the registers.
-// /// Note that those functions should mostly just be implemented and not used since they are not bound by Read/Write/Edit permission.
-// pub trait RegisterInterface<R, A, E>
-// where
-//     R: Register<Address = A, Error = E>,
-// {
-//     /// The return type of the read_register function
-//     type ReadOutput = Future<Output = Result<R, R::Error>>;
+use device_register::{EditableRegister, ReadableRegister, Register, WritableRegister};
+use futures::Future;
 
-//     /// Reads a register and returns it
-//     fn read_register(&mut self) -> ReadOutput;
+/// Traits that define how to read and write the registers.
+/// Note that those functions should mostly just be implemented and not used since they are not bound by Read/Write/Edit permission.
+pub trait RegisterInterface<R, A, E>
+where
+    R: Register<Address = A, Error = E>,
+{
+    /// The return type of the read_register function
+    type ReadOutput<'a>: Future<Output = Result<R, E>>
+    where
+        Self: 'a;
 
-//     /// The return type of the write_register function
-//     type WriteOutput = Future<Output = Result<R, R::Error>>;
+    /// Reads a register and returns it
+    fn read_register<'a>(&'a mut self) -> Self::ReadOutput<'a>;
 
-//     /// Writes a register to the device
-//     fn write_register(&mut self, register: &R) -> WriteOutput;
-// }
+    /// The return type of the write_register function
+    type WriteOutput<'a>: Future<Output = Result<(), E>>
+    where
+        Self: 'a;
 
-// /// Trait to safely read a register. Only a readable register can be read.
-// pub trait ReadRegister<R, A, E>
-// where
-//     R: ReadableRegister<Address = A, Error = E>,
-// {
-//     /// Read a register
-//     fn read(&mut self) -> Result<R, R::Error>;
-// }
+    /// Writes a register to the device
+    fn write_register<'a>(&'a mut self, register: &R) -> Self::WriteOutput<'a>;
+}
 
-// /// Trait to safely write a register. Only a writable register can be written to.
-// pub trait WriteRegister<R, A, E>
-// where
-//     R: WritableRegister<Address = A, Error = E>,
-// {
-//     /// Write a register
-//     fn write(&mut self, register: R) -> Result<(), R::Error>;
-// }
+/// Trait to safely read a register. Only a readable register can be read.
+pub trait ReadRegister<R, A, E>
+where
+    R: ReadableRegister<Address = A, Error = E>,
+{
+    /// The return type of the read
+    type Output<'a>: Future<Output = Result<R, E>>
+    where
+        Self: 'a;
 
-// /// Trait to safely read-edit-write a register.
-// /// Usefull when a register has reserved values for internal uses.
-// /// Avoids writing garbage to the reserved  bits.
-// pub trait EditRegister<R, A, E>
-// where
-//     R: EditableRegister<Address = A, Error = E>,
-// {
-//     /// Edit a register. The closure takes a reference to the register,
-//     /// the same register must be edited, then returned.
-//     fn edit<F>(&mut self, f: F) -> Result<(), R::Error>
-//     where
-//         for<'w> F: FnOnce(&'w mut R) -> &'w mut R;
-// }
+    /// Read a register
+    fn read<'a>(&'a mut self) -> Self::Output<'a>;
+}
 
-// impl<I, R, A, E> ReadRegister<R, A, E> for I
-// where
-//     R: ReadableRegister<Address = A, Error = E>,
-//     I: RegisterInterface<R, A, E>,
-// {
-//     fn read(&mut self) -> Result<R, R::Error> {
-//         self.read_register()
-//     }
-// }
+/// Trait to safely write a register. Only a writable register can be written to.
+pub trait WriteRegister<R, A, E>
+where
+    R: WritableRegister<Address = A, Error = E>,
+{
+    /// The return type of the write
+    type Output<'a>: Future<Output = Result<(), E>>
+    where
+        Self: 'a;
 
-// impl<I, R, A, E> WriteRegister<R, A, E> for I
-// where
-//     R: WritableRegister<Address = A, Error = E>,
-//     I: RegisterInterface<R, A, E>,
-// {
-//     fn write(&mut self, register: R) -> Result<(), R::Error> {
-//         self.write_register(&register)
-//     }
-// }
+    /// Write a register
+    fn write<'a>(&'a mut self, register: R) -> Self::Output<'a>;
+}
 
-// impl<I, R, A, E> EditRegister<R, A, E> for I
-// where
-//     R: EditableRegister<Address = A, Error = E>,
-//     I: RegisterInterface<R, A, E>,
-// {
-//     fn edit<F>(&mut self, f: F) -> Result<(), R::Error>
-//     where
-//         for<'w> F: FnOnce(&'w mut R) -> &'w mut R,
-//     {
-//         let mut val = self.read_register()?;
-//         f(&mut val);
-//         self.write_register(&val)
-//     }
-// }
+/// Trait to safely read-edit-write a register.
+/// Usefull when a register has reserved values for internal uses.
+/// Avoids writing garbage to the reserved  bits.
+pub trait EditRegister<R, A, E>
+where
+    R: EditableRegister<Address = A, Error = E>,
+{
+    /// The return type of the write
+    type Output<'a>: Future<Output = Result<(), E>>
+    where
+        Self: 'a;
+
+    /// Edit a register. The closure takes a reference to the register,
+    /// the same register must be edited, then returned.
+    fn edit<'a, F>(&'a mut self, f: F) -> Self::Output<'a>
+    where
+        for<'w> F: FnOnce(&'w mut R) -> &'w mut R;
+}
+
+impl<I, R, A, E> ReadRegister<R, A, E> for I
+where
+    for<'a> R: ReadableRegister<Address = A, Error = E> + 'a,
+    I: RegisterInterface<R, A, E>,
+    for<'a> A: 'a,
+    for<'a> E: 'a,
+{
+    type Output<'a> = impl Future<Output = Result<R, E>> +'a where Self: 'a;
+
+    fn read<'a>(&'a mut self) -> Self::Output<'a> {
+        RegisterInterface::read_register(self)
+    }
+}
--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
 
-        rustpkg = pkgs.rust-bin.stable.latest.default;
+        rustpkg = pkgs.rust-bin.nightly.latest.default;
 
       in with pkgs; { devShell = mkShell { buildInputs = [ rustpkg ]; }; });
 }
