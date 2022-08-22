77434828b0184630ece0403473a5753f959d66c5
xgroleau 🐢 <xavgroleau@gmail.com>
2022-08-22 10:17:58 -0400

    Added some doc for the lib

--- device-register/src/lib.rs
+++ device-register/src/lib.rs
@@ -1,4 +1,84 @@
-//! A library to ease the manipulation of writing register mapping for drivers
+//! # device-register
+//! A toolkit to describe the register of your devies to ease driver development
+//!
+//! ## Example
+//! ```rust
+//! use std::collections::HashMap;
+//! use device_register::*;
+//!
+//! // The type of the address used by the driver
+//! pub struct Address(pub u8);
+//!
+//! // The type of the error
+//! pub type DeviceError = ();
+//!
+//! // We define the register with Read/Write permission
+//! // Then we pass the address type, value and error type of the driveer
+//! #[derive(Debug, Copy, PartialEq, Eq, Clone, RWRegister)]
+//! #[register( addr = "Address(1)", ty = "Address", err = "DeviceError" )]
+//! pub struct Register0(pub u16);
+//! # impl From<Register0> for u16 {
+//! #     fn from(val: Register0) -> Self {
+//! #        val.0
+//! #     }
+//! # }
+//! # impl From<u16> for Register0 {
+//! #     fn from(val: u16) -> Self {
+//! #         Register0(val)
+//! #     }
+//! # }
+//!
+//! // Mock of the device driver
+//! pub struct DeviceDriver {
+//!     // Simulate reading from the device
+//!     pub registers: HashMap<u8, u16>,
+//! }
+//!
+//! // We implement the required interface
+//! impl<R> RegisterInterface<R, Address, DeviceError> for DeviceDriver
+//! where
+//!     R: Register<Address = Address, Error = DeviceError> + Clone + From<u16>,
+//!     u16: From<R>,
+//! {
+//!     fn read_register(&mut self) -> Result<R, DeviceError> {
+//!         let bytes = self.registers.get(&R::ADDRESS.0).unwrap();
+//!         Ok(bytes.clone().into())
+//!     }
+//!
+//!     fn write_register(&mut self, register: &R) -> Result<(), DeviceError> {
+//!         self.registers.insert(R::ADDRESS.0, register.clone().into());
+//!         Ok(())
+//!     }
+//! }
+//!
+//! let mut device = DeviceDriver{
+//!     registers:  HashMap::new(),
+//! };
+//! let write = Register0(42);
+//! device.write(write).unwrap();
+//!
+//! let read: Register0 = device.read().unwrap();
+//!
+//! assert_eq!(read, write);
+//!
+//! device.edit(|r: &mut Register0| {
+//!     r.0 = 43;
+//!     r
+//! }).unwrap();
+//!
+//! let read: Register0 = device.read().unwrap();
+//! assert_eq!(read, Register0(43));
+//!
+//! ```
+//!
+//! ## License
+//! Licensed under either of
+//! - Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+//!   http://www.apache.org/licenses/LICENSE-2.0)
+//!
+//! - MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+//!
+//! at your option.
 #![no_std]
 #![deny(unsafe_code, missing_docs)]
 
